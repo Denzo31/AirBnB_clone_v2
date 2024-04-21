0a899600d35761d133ad4d254e0cbdb7ad37c6d7
Denzo31 <shederrick03@gmail.com>
2024-04-21 15:51:43 -0700

    6 odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """ Display HBNB """
-    return "HBNB"
+    """ Display Hello HBNB! """
+    return 'Hello HBNB!'
 
 
 @app.route("/hbnb", strict_slashes=False)
