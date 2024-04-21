56a75ca11baba8d467f846b222c793fada44c956
Denzo31 <shederrick03@gmail.com>
2024-04-21 13:35:48 -0700

    00

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
+""" Minimal Flask app """
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def hello():
+    """ Display Hello HBNB! """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """ Display HBNB """
     return 'HBNB'
+
 
 if__name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
